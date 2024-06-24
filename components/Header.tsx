57d5432b7a6b094f57c8a4673019a7c6af1d7d6e
Elizabet Oliveira <2750668+miukimiu@users.noreply.github.com>
2024-06-24 16:25:40 +0100

    Fixing nav for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,19 +3,15 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import NextImage from "next/image";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { syncopate } from "@/utils/fonts";
 
 export const Header = () => {
   const pathname = usePathname();
+  const router = useRouter();
 
   // State to manage the navbar's visibility
-  const [nav, setNav] = useState(false);
-
-  // Toggle function to handle the navbar's display
-  const handleNav = () => {
-    setNav(!nav);
-  };
+  const [isNavVisible, setIsNavVisibe] = useState(false);
 
   // Array containing navigation items
   const navItems = [
@@ -25,6 +21,12 @@ export const Header = () => {
     { id: 4, text: "Eventos", href: "/eventos" },
     { id: 5, text: "Contactos", href: "/contactos" },
   ];
+
+  const onClickPageLink = (href: string) => {
+    router.push(href);
+
+    setIsNavVisibe(false);
+  };
 
   return (
     <nav
@@ -50,38 +52,49 @@ export const Header = () => {
       </ul>
 
       {/* Mobile Navigation Icon */}
-      <div onClick={handleNav} className="block md:hidden relative">
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      <div onClick={() => setIsNavVisibe(true)}>
+        <AiOutlineMenu size={20} />
       </div>
 
       {/* Mobile Navigation Menu */}
-      <ul
+      <div
         className={
-          nav
+          isNavVisible
             ? "fixed md:hidden left-0 top-0 w-full h-full ease-in-out duration-500 bg-zinc-950"
             : "ease-in-out w-full duration-500 fixed top-0 bottom-0 left-[-100%]"
         }
       >
-        {/* Mobile Logo */}
-        <h1 className="w-full p-4">
-          <Link href="/">
-            <NextImage alt="" src="img/logo.svg" width={287} height={21} />
-          </Link>
-        </h1>
+        <div className="flex justify-between items-center h-24 max-w-screen-xl mx-auto px-4 text-white w-full">
+          <h1 className="w-full">
+            <Link href="/">
+              <NextImage alt="" src="img/logo.svg" width={287} height={21} />
+            </Link>
+          </h1>
 
-        {/* Mobile Navigation Items */}
-        {navItems.map((item) => (
-          <Link href={item.href} key={item.id}>
-            <li
-              className={`p-4 duration-300 cursor-pointer uppercase text-6xl font-bold  ${
-                syncopate.className
-              } ${pathname === item.href ? "text-white'" : "text-trail"}`}
+          <div onClick={() => setIsNavVisibe(false)}>
+            <AiOutlineClose size={20} />
+          </div>
+        </div>
+
+        <ul>
+          {/* Mobile Navigation Items */}
+          {navItems.map((item) => (
+            <Link
+              href={item.href}
+              key={item.id}
+              onClick={() => onClickPageLink(item.href)}
             >
-              {item.text}
-            </li>
-          </Link>
-        ))}
-      </ul>
+              <li
+                className={`p-4 duration-300 cursor-pointer uppercase text-4xl font-bold  ${
+                  syncopate.className
+                } ${pathname === item.href ? "text-white'" : "text-trail"}`}
+              >
+                {item.text}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
