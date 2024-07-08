d5261538c2c0f842c043e23f0804268a6101095c
Elizabet Oliveira <2750668+miukimiu@users.noreply.github.com>
2024-07-08 10:30:27 +0100

    Remove unnecessary component

--- components/Badge.tsx
+++
@@ -1,9 +0,0 @@
-export const Badge = ({ children, className }) => {
-  return (
-    <span
-      className={`bg-zinc-100 text-zinc-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-zinc-700 dark:text-zinc-300 ${className}`}
-    >
-      {children}
-    </span>
-  );
-};
