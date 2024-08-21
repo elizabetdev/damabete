5243b168155f1a9ea43466f31f9fd1af6595140d
Elizabet Oliveira <2750668+miukimiu@users.noreply.github.com>
2024-08-21 19:04:39 +0100

    Remove stats

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,12 @@
 import { Hero } from "@/components/Hero";
 import React from "react";
 import { DestaquesRecentes } from "@/components/DestaquesRecentes";
-import { Stats } from "@/components/Stats";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <DestaquesRecentes />
-      <Stats />
     </main>
   );
 }
